ed14aae0abdfd1686c1a9502767d57ac0592aee3
Skyggfisk <mahasi93@hotmail.com>
2017-10-17 12:39:17 +0200

    Transpiled and made sure the skeleton ran

---
+++ app/app.js
@@ -0,0 +1,42 @@
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const index_1 = require("./routes/index");
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', index_1.default);
+app.use((req, res, next) => {
+    var err = new Error('Not Found');
+    err['status'] = 404;
+    next(err);
+});
+if (process.env.NODE_ENV === 'development') {
+    app.use((err, req, res, next) => {
+        res.status(err['status'] || 500);
+        res.render('error', {
+            title: 'error',
+            message: err.message,
+            error: err
+        });
+    });
+}
+app.use((err, req, res, next) => {
+    res.status(err['status'] || 500);
+    res.render('error', {
+        title: 'error',
+        message: err.message,
+        error: {}
+    });
+});
+exports.default = app;
+//# sourceMappingURL=app.js.map
