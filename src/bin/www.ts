cc67c6876d8e8fe698e58057b9fa33b7dd078f87
Skyggfisk <mahasi93@hotmail.com>
2017-11-29 09:56:12 +0100

    adwada

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -9,17 +9,17 @@ import * as https from "https";
 import * as fs from "fs";
 
 // ssl things
-var key = fs.readFileSync("/home/ubuntu/private.key");
-var cert = fs.readFileSync("/home/ubuntu/primary.crt");
-var ca = fs.readFileSync("/home/ubuntu/server.crt");
+// var key = fs.readFileSync("/home/ubuntu/private.key");
+// var cert = fs.readFileSync("/home/ubuntu/primary.crt");
+// var ca = fs.readFileSync("/home/ubuntu/server.crt");
 
-var SSLoptions = {
-  key: key,
-  cert: cert,
-  ca: ca
-};
+// var SSLoptions = {
+//   key: key,
+//   cert: cert,
+//   ca: ca
+// };
 
-https.createServer(SSLoptions, app).listen(443);
+// https.createServer(SSLoptions, app).listen(443);
 
 /**
  * Get port from environment and store in Express.
