eb7cf40abca781dfa356d1264cc83e525fefb6c3
Michael Norup <1058717@ucn.dk>
2017-11-29 09:50:49 +0100

    qwe

--- app/models/conversation.js
+++ app/models/conversation.js
@@ -4,11 +4,7 @@ const mongoose_1 = require("mongoose");
 const user_1 = require("../models/user");
 var ConversationSchema = new mongoose_1.Schema({
     messages: Array,
-<<<<<<< HEAD
-    user: user_1.User
-=======
     user: [{ type: mongoose_1.Schema.Types.ObjectId, ref: user_1.User }]
->>>>>>> 21b4efabdeb867250faeb2c7f9a8bb3f3bdf49f3
 });
 exports.Conversation = mongoose_1.model("Conversation", ConversationSchema);
 exports.default = exports.Conversation;
